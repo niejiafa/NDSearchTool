62d3f5ac7766dba76c2baf454aee4142fd22594b
jiafa <18321274679@163.com>
2016-03-08 16:55:06 +0800

    add custom search UI
    add custom search function UI

---
+++ NDSearchTool/CustomSearchViewController/NDCustomDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  NDCustomDemoViewController.h
+//  NDSearchTool
+//
+//  Created by NDMAC on 16/3/8.
+//  Copyright © 2016年 NDEducation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NDCustomDemoViewController : UIViewController
+
+@end
---
+++ NDSearchTool/CustomSearchViewController/NDCustomSearchViewController.m
@@ -0,0 +1,29 @@
+//
+//  NDCustomSearchViewController.m
+//  NDSearchTool
+//
+//  Created by NDMAC on 16/3/8.
+//  Copyright © 2016年 NDEducation. All rights reserved.
+//
+
+#import "NDCustomSearchViewController.h"
+
+@interface NDCustomSearchViewController ()
+
+@end
+
+@implementation NDCustomSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
